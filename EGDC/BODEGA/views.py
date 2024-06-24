d5a6eeddb4f9b5c8779895a3e4fd5b21582ab932
ElGueydelCalzado <elweydelcalzado@gmail.com>
2024-06-24 04:28:02 +0000

    SKU list not updating properly

--- EGDC/BODEGA/views.py
+++ EGDC/BODEGA/views.py
@@ -1,9 +1,20 @@
-from django.http import HttpResponse
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+
+class NewSkuForm(forms.Form):
+    sku = forms.CharField(label="Nuevo SKU")
 
 # Create your views here.
 def index(request):
-    return render(request, "bodega/index.html")
+    if "BODEGA" not in request.session:
+        request.session["skus"] = []
+
+    return render(request, "bodega/index.html", {
+        "sku": request.session["skus"]
+    })
 
 def searchSKU(request, sku):
     return render(request, "bodega/index.html", {
@@ -11,4 +22,16 @@ def searchSKU(request, sku):
     })
 
 def addSKU(request):
-    return HttpResponse("Page to Request to ADD a new Item!!")
+    if request.method == "POST":
+        form = NewSkuForm(request.POST)
+        if form.is_valid():
+            sku = form.cleaned_data["sku"]
+            request.session["skus"] += [sku]
+            return HttpResponseRedirect(reverse("bodega:index"))
+        else:
+            return render(request, "bodega/addSKU.html", {
+                "form": form
+            })
+    return render(request, "bodega/addSKU.html", {
+        "form": NewSkuForm()
+    })
